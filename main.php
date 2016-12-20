7512e6b4d43fa58ce47e2a54afe8ccb51a7844c8
Vincenzo Trapani <vincenzo.trapani@amara.com>
2016-12-20 12:42:53 +0000

    Main run

--- main.php
+++ main.php
@@ -1,1 +1,23 @@
 <?php
+
+use MineFields\Field\Builder\FieldBuilder;
+use MineFields\Field\FieldManager;
+use MineFields\Solver;
+
+require_once 'vendor/autoload.php';
+
+$given = <<<EOD
+3 3
+*..
+..*
+...
+EOD;
+
+$field = new FieldBuilder();
+$field->setInput($given);
+
+$solver = new Solver(new FieldManager());
+$solved = $solver->solve($field->build());
+
+$output = new MineFields\Output\Formatter\SimpleTextFormatter();
+print $output->format($solved);
