057dd521947a23c68ff9dc828cf0ed76b3f74792
Vincenzo Trapani <vincenzo.trapani@amara.com>
2016-12-20 12:30:02 +0000

    Added a unit and integation test for the solver

---
+++ tests/Integration/MineFields/SolverTest.php
@@ -0,0 +1,46 @@
+<?php
+namespace Tests\Integration\MineFields;
+
+use MineFields\Field\FieldManager;
+use MineFields\Field\Object\Cell;
+use MineFields\Field\Object\Field;
+use MineFields\Field\Object\Row;
+use MineFields\Solver;
+
+class SolverTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSolverWillReturnTheRightSolvedField()
+    {
+        $solver = new Solver(new FieldManager());
+
+        $expected = new Field(3, 4, [
+            new Row([new Cell(2, 0, 0), new Cell('*', 1, 0), new Cell(1, 2, 0)]),
+            new Row([new Cell('*', 0, 1), new Cell(2, 1, 1), new Cell(1, 2, 1)]),
+            new Row([new Cell(1, 0, 2), new Cell(2, 1, 2), new Cell(1, 2, 2)]),
+            new Row([new Cell(0, 0, 3), new Cell(1, 1, 3), new Cell('*', 2, 3)]),
+        ]);
+
+        $given = new Field(3, 4, [
+            new Row([new Cell('.', 0, 0), new Cell('*', 1, 0), new Cell('.', 2, 0)]),
+            new Row([new Cell('*', 0, 1), new Cell('.', 1, 1), new Cell('.', 2, 1)]),
+            new Row([new Cell('.', 0, 2), new Cell('.', 1, 2), new Cell('.', 2, 2)]),
+            new Row([new Cell('.', 0, 3), new Cell('.', 1, 3), new Cell('*', 2, 3)]),
+        ]);
+
+        $this->assertEquals($expected, $solver->solve($given));
+
+        $expected = new Field(3, 3, [
+            new Row([new Cell(1, 0, 0), new Cell(1, 1, 0), new Cell(0, 2, 0)]),
+            new Row([new Cell('*', 0, 1), new Cell(2, 1, 1), new Cell(1, 2, 1)]),
+            new Row([new Cell(2, 0, 2), new Cell('*', 1, 2), new Cell(1, 2, 2)]),
+        ]);
+
+        $given = new Field(3, 3, [
+            new Row([new Cell('.', 0, 0), new Cell('.', 1, 0), new Cell('.', 2, 0)]),
+            new Row([new Cell('*', 0, 1), new Cell('.', 1, 1), new Cell('.', 2, 1)]),
+            new Row([new Cell('.', 0, 2), new Cell('*', 1, 2), new Cell('.', 2, 2)]),
+        ]);
+
+        $this->assertEquals($expected, $solver->solve($given));
+    }
+}
--- tests/Unit/MineFields/SolverTest.php
+++ tests/Unit/MineFields/SolverTest.php
@@ -1,4 +1,6 @@
 <?php
+
+
 namespace Tests\Unit\MineFields;
 
 use MineFields\Field\FieldManager;
@@ -6,42 +8,37 @@ use MineFields\Field\Object\Cell;
 use MineFields\Field\Object\Field;
 use MineFields\Field\Object\Row;
 use MineFields\Solver;
+use Prophecy\Argument;
 
 class SolverTest extends \PHPUnit_Framework_TestCase
 {
-    public function testSolverWillReturnTheRightSolvedField()
+    public function testSolverWillReturnTheRightFieldSchema()
     {
-        $solver = new Solver(new FieldManager());
-
-        $expected = new Field(3, 4, [
-            new Row([new Cell(2, 0, 0), new Cell('*', 1, 0), new Cell(1, 2, 0)]),
-            new Row([new Cell('*', 0, 1), new Cell(2, 1, 1), new Cell(1, 2, 1)]),
-            new Row([new Cell(1, 0, 2), new Cell(2, 1, 2), new Cell(1, 2, 2)]),
-            new Row([new Cell(0, 0, 3), new Cell(1, 1, 3), new Cell('*', 2, 3)]),
-        ]);
-
-        $given = new Field(3, 4, [
-            new Row([new Cell('.', 0, 0), new Cell('*', 1, 0), new Cell('.', 2, 0)]),
-            new Row([new Cell('*', 0, 1), new Cell('.', 1, 1), new Cell('.', 2, 1)]),
-            new Row([new Cell('.', 0, 2), new Cell('.', 1, 2), new Cell('.', 2, 2)]),
-            new Row([new Cell('.', 0, 3), new Cell('.', 1, 3), new Cell('*', 2, 3)]),
-        ]);
-
-        $this->assertEquals($expected, $solver->solve($given));
-
-        $expected = new Field(3, 3, [
-            new Row([new Cell(1, 0, 0), new Cell(1, 1, 0), new Cell(0, 2, 0)]),
-            new Row([new Cell('*', 0, 1), new Cell(2, 1, 1), new Cell(1, 2, 1)]),
-            new Row([new Cell(2, 0, 2), new Cell('*', 1, 2), new Cell(1, 2, 2)]),
-
-        ]);
-
         $given = new Field(3, 3, [
             new Row([new Cell('.', 0, 0), new Cell('.', 1, 0), new Cell('.', 2, 0)]),
             new Row([new Cell('*', 0, 1), new Cell('.', 1, 1), new Cell('.', 2, 1)]),
             new Row([new Cell('.', 0, 2), new Cell('*', 1, 2), new Cell('.', 2, 2)]),
         ]);
 
-        $this->assertEquals($expected, $solver->solve($given));
+
+        $solver = new Solver($this->getFieldManager());
+        $solved = $solver->solve($given);
+
+        $this->assertInstanceOf(Field::class, $solved);
+        $this->assertCount(3, $solved->getData());
+        $this->assertEquals(3, $solved->getColumns());
+    }
+
+    /**
+     * @return FieldManager
+     */
+    private function getFieldManager()
+    {
+        $fieldManager = $this->prophesize(FieldManager::class);
+        $fieldManager->getCellByPosition(Argument::type(Field::class), Argument::type('int'), Argument::type('int'))
+            ->will(function ($args) {
+                return new Cell($args[0], $args[1], $args[2]);
+            });
+        return $fieldManager->reveal();
     }
 }
